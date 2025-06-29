c8f9ee68ac94487ebd242f0c4fd670d4d315a706
jhorlodev <131390416+Jhorlodev@users.noreply.github.com>
2025-06-29 02:31:54 -0400

    tutas

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [react()],
   site:"https://jhorlodev.github.io",
-/* base:"/control/",*/
+ base:"/control.github.io/"
 });
