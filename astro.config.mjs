2e602583c4f9fe02a7097001d88bd00d13fe3d5f
jhorlodev <131390416+Jhorlodev@users.noreply.github.com>
2025-07-04 02:40:56 -0400

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [react()],
   site:"https://jhorlodev.github.io",
- /* base:"/control.github.io/"*/
+  base:"/control.github.io/"
 });
